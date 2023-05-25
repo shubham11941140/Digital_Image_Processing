7558085832100e873a0ed365cba7a4d08496af18
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2023-05-25 11:14:33 +0530

    Q1 done

---
+++ Q1/Q1_Test_Script.py
@@ -0,0 +1,14 @@
+# Refer to Q1.ipynb for assumptions, code documentation and implementation of the function.
+# There is a section which is used to invoke the functions for testing purposes. You can change the parameters as you wish for manual test cases within the Q1.ipynb file.
+
+# Parameters : (Manual Testing)
+# mat=image matrix containing intensity values
+# p=start point
+# q=end point
+# V=set of intensities for which the path should be extracted
+# adj=type of path requested by the user where I have defined adj1 for 4-path, adj2 for 8-path and adj3 for m-path.
+
+# There are some examples given within the IPYNB that deal with the changing of parameters. You can change the parameters with your input and run it.
+
+# Ensure the arguments that you pass to the function are in the correct order and are valid and of the correct type.
+# Otherwise the program will throw an error and exit.
